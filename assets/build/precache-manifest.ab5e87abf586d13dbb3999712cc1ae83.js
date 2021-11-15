c40693dd532f93e7bf27f6a57fba6184cf51ec07
Kevin <kchensu@sfu.ca>
2021-11-14 18:01:56 -0800

    build project

---
+++ assets/build/precache-manifest.ab5e87abf586d13dbb3999712cc1ae83.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e29cce187a589670dd30a49d3e2ed2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d56b6035f8a3a63d72bd",
+    "url": "/static/css/2.1c7370f6.chunk.css"
+  },
+  {
+    "revision": "ada6a6e9dda3d29050a0",
+    "url": "/static/css/main.4b118a85.chunk.css"
+  },
+  {
+    "revision": "d56b6035f8a3a63d72bd",
+    "url": "/static/js/2.c93548d5.chunk.js"
+  },
+  {
+    "revision": "7e691de90681f28b2538150436e19c7c",
+    "url": "/static/js/2.c93548d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada6a6e9dda3d29050a0",
+    "url": "/static/js/main.20c6b569.chunk.js"
+  },
+  {
+    "revision": "fc57411f62abc7420ae5",
+    "url": "/static/js/runtime-main.9280458a.js"
+  },
+  {
+    "revision": "e99fad78c474a7a490717058994ba24c",
+    "url": "/static/media/Erin.e99fad78.png"
+  },
+  {
+    "revision": "ba959c5132cca492ff9a7f410fc84671",
+    "url": "/static/media/Jacob.ba959c51.png"
+  },
+  {
+    "revision": "f251214e4c9fe4dfc4e09bbfc6d7285e",
+    "url": "/static/media/John.f251214e.png"
+  },
+  {
+    "revision": "f95a983ab2893a069c782d92ee6b9a70",
+    "url": "/static/media/bestSellers.f95a983a.png"
+  },
+  {
+    "revision": "161c4a441a391c6bc596220c8db0a1e4",
+    "url": "/static/media/burgers.161c4a44.png"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "67a259630b758587c7af9cc114156615",
+    "url": "/static/media/hero-cars.67a25963.png"
+  },
+  {
+    "revision": "f06e69a73260b2bea082c6169d7a09f7",
+    "url": "/static/media/hero-cookbooks.f06e69a7.png"
+  },
+  {
+    "revision": "57fafec1ff8ba0843ea9d535501bf06d",
+    "url": "/static/media/hero-database.57fafec1.png"
+  },
+  {
+    "revision": "ec14c774c54de97e0e06a2bff4a6bf9a",
+    "url": "/static/media/hero-fairytales.ec14c774.png"
+  },
+  {
+    "revision": "837f6ae970360aedb7befb250749a10b",
+    "url": "/static/media/hero-home.837f6ae9.png"
+  },
+  {
+    "revision": "1c7e5e038d8d4dfcd53ff7c8b73297e6",
+    "url": "/static/media/hero-main.1c7e5e03.png"
+  },
+  {
+    "revision": "b62da3709e287d7973e1a9af5601e5c6",
+    "url": "/static/media/hero-science.b62da370.png"
+  },
+  {
+    "revision": "3df92e23165897152ea24986d7181f0e",
+    "url": "/static/media/hero-woodwork.3df92e23.png"
+  },
+  {
+    "revision": "922459532b985f95bcbbf46428a6093f",
+    "url": "/static/media/italian.92245953.png"
+  },
+  {
+    "revision": "86634adf1691832e085e24841bf1c183",
+    "url": "/static/media/noodles.86634adf.png"
+  },
+  {
+    "revision": "22f42738e6496c5085dfa50133218292",
+    "url": "/static/media/pancakes.22f42738.png"
+  },
+  {
+    "revision": "9fa03bfe92fc72fea97174e72e1e97d4",
+    "url": "/static/media/pastOrders.9fa03bfe.png"
+  },
+  {
+    "revision": "203d8247e56a2f1c0fd7d52baf1b19c3",
+    "url": "/static/media/pineapple.203d8247.png"
+  },
+  {
+    "revision": "e45e4325617237f3355f5eb47d2a8908",
+    "url": "/static/media/screenshot.e45e4325.png"
+  },
+  {
+    "revision": "0f89c06edb2fb41d5abcae96cf8281d5",
+    "url": "/static/media/supportedCards.0f89c06e.png"
+  },
+  {
+    "revision": "c6f148dcb9b1d22b54c5177dc63a2996",
+    "url": "/static/media/umami.c6f148dc.png"
+  },
+  {
+    "revision": "76f37cd9c73a4a657f53ac2c301dc74b",
+    "url": "/static/media/yourpastorders.76f37cd9.png"
+  },
+  {
+    "revision": "39c06f6190125eb15c8fbd6abd2574aa",
+    "url": "/static/media/yourshoppingcart.39c06f61.png"
+  }
+]);
